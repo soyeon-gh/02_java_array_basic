f69b0ca2fbb3e22af49214d3d43009a48f966016
19시_웹개발_ksy <19__ksy@DESKTOP-F0Q81EV>
2021-02-09 19:39:31 +0900

    commit

---
+++ 02_java_array_basic/src/step2_01/array/ArrayEx01.java
@@ -0,0 +1,29 @@
+package step2_01.array;
+
+public class ArrayEx01 {
+
+	public static void main(String[] args) {
+
+		// 1. 일반 변수 ( 값 저장 ) 
+		int nVar = 0;
+		nVar = 3;
+		System.out.println(nVar);		// 값
+		
+		// 2. 주소 변수 ( 주소 저장 )
+		int[] arr = null;
+		arr = new int[5]; 
+		System.out.println(arr);		// 주소
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		System.out.println();
+		System.out.println();
+		
+		
+		
+	}
+
+}
