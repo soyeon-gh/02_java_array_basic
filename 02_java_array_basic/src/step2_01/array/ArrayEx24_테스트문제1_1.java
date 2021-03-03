33d8f123d14cd927ef0f4b8c47b7458338416499
19시_웹개발_ksy <19__ksy@DESKTOP-F0Q81EV>
2021-03-03 21:33:24 +0900

    commit

--- 02_java_array_basic/src/step2_01/array/ArrayEx24_테스트문제1_1.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx24_테스트문제1_1.java
@@ -1,35 +1,179 @@
 package step2_01.array;
 
+import java.util.Scanner;
+
+//2021-03-02 19:00
+
 public class ArrayEx24_테스트문제1_1 {
 
 	public static void main(String[] args) {
-		
-		
+
+		Scanner sc = new Scanner(System.in);
+
 		int[] a = { 10, 4, 5, 3, 1 };
-		
-		// 문제 1) 위 배열를 이용해서 전체 요소의 합출력
-		
-		// 문제 2) 값을 입력하면 인덱스 출력
-		// 예) 5 ==> 2  ,  1 ==> 4
-		
-		// 문제 3)  인덱스를 입력하면 값 출력
-		// 예) 2 ==> 5  , 4 ==> 1
+		int sum = 0;
+		int check = -1;
+		int index = 0;
+		int cArrayIndexNumber = 0;
+		int max = 0;
+		int cnt = 0;
 
-		// 문제 4) 위 배열중 가장 큰 값 출력 
+		System.out.println("┌───────────────────────────────────────────────┐");
+		System.out.println("│            ArrayEx24_테스트문제1_1            │");
+		System.out.println("└───────────────────────────────────────────────┘");
+
+		// 문제 1) 위 배열를 이용해서 전체 요소의 합출력 /////////////////////////////
+
+		System.out.println(" 문제 1) 위 배열를 이용해서 전체 요소의 합출력");
+System.out.println();
+		for (int i = 0; i < a.length; i++) {
+			sum += a[i];
+		}
+
+		System.out.println(" 답 : " + sum);
+
+		// 문제 2) 값을 입력하면 인덱스 출력 /////////////////////////////////////
+		// 예) 5 ==> 2 , 1 ==> 4
+		System.out.println("───────────────────────────────────────────────");
+		System.out.println(" 문제 2) 값을 입력하면 인덱스 출력             ");
+
+		System.out.print("  { ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]);
+			if (i < a.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println(" }");
+		System.out.println();
+		System.out.print(" 값 입력 : ");
+		int getValue = sc.nextInt();
+
+		for (int i = 0; i < a.length; i++) {
+			if (getValue == a[i]) {
+				check = 0;
+				index = i;
+			}
+		}
+
+		if (check == 0) {
+			System.out.println(" 값 : " + getValue + " ==> 인덱스 : " + index);
+		} else if (check == -1) {
+			System.out.println("┌──────────────────┐");
+			System.out.println("│  없는 값 입니다  │");
+			System.out.println("└──────────────────┘");
+		}
+
+		System.out.println();
+
+		// 문제 3) 인덱스를 입력하면 값 출력 /////////////////////////////////////////
+		// 예) 2 ==> 5 , 4 ==> 1
+
+		System.out.println("───────────────────────────────────────────────");
+		System.out.println(" 문제 3) 인덱스를 입력하면 값 출력");
+
+		System.out.print("{ ");
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(a[i]);
+			if (i < a.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println(" }");
+		System.out.println();
+		System.out.print(" 인덱스 입력 : ");
+		int getIndexNumber = sc.nextInt();
+
+		if (getIndexNumber <= a.length) {
+			for (int i = 0; i < a.length; i++) {
+				if (a[getIndexNumber] == a[i]) {
+					System.out.println(" 인덱스 : " + getIndexNumber + " ==> 값 : " + a[i]);
+				}
+			}
+		} else if (getIndexNumber > a.length || getIndexNumber < 0) {
+			System.out.println("┌──────────────────────┐");
+			System.out.println("│  없는 인덱스 입니다  │");
+			System.out.println("└──────────────────────┘");
+		}
+
+		System.out.println();
+
+		// 문제 4) 위 배열중 가장 큰 값 출력 /////////////////////////////////////////
 		// 예) 10
+		System.out.println("───────────────────────────────────────────────");
+		System.out.println(" 문제 4) 위 배열중 가장 큰 값 출력");
+		System.out.println();
+		for (int i = 0; i < a.length; i++) {
+			if (max < a[i]) {
+				max = a[i];
+			}
+		}
 
-		// 문제 5) 위 배열중 홀수의 개수 출력 
+		System.out.println(" 답 : " + max);
+		System.out.println();
+
+		// 문제 5) 위 배열중 홀수의 개수 출력 /////////////////////////////////////////
 		// 예) 홀수의 개수 : 3
-			
-		// 문제 6) a의 값중 홀수만 b에 저장 (저장위치는 a와 b의 동일한 위치에 저장)
-		// 예) b => {0, 0, 5, 3, 1}
-		int[] b = {0, 0, 0, 0, 0};
-		
-		// 문제 7) 다음 리스트를 이용해서 a의 값중 홀수만 c에 저장(저장위치는 앞에서부터  저장 , 3번 문제와 다름)
-		// 예) b => {5, 3, 1, 0, 0}
-		int[] c = { 0,0,0,0,0 };
+		System.out.println("───────────────────────────────────────────────");
+		System.out.println(" 문제 5) 위 배열중 홀수의 개수 출력");
+		System.out.println();
+		for (int i = 0; i < a.length; i++) {
+			if (a[i] % 2 == 1) {
+				cnt++;
+			}
+		}
 
-		
+		System.out.println(" 답 : " + cnt);
+		System.out.println();
+
+		// 문제 6) a의 값중 홀수만 b에 저장 /////////////////////////////////////////
+		// (저장위치는 a와 b의 동일한 위치에 저장)
+		// 예) b => {0, 0, 5, 3, 1}
+
+		int[] b = { 0, 0, 0, 0, 0 };
+		System.out.println("───────────────────────────────────────────────");
+		System.out.println(" 문제 6) a의 값중 홀수만 b에 저장");
+		System.out.println();
+		System.out.print(" 답 : { ");
+		for (int i = 0; i < b.length; i++) {
+			if (a[i] % 2 == 1) {
+				b[i] = a[i];
+			}
+			System.out.print(b[i]);
+			if (i < b.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println(" }");
+		System.out.println();
+
+		// 문제 7) 다음 리스트를 이용해서 /////////////////////////////////////////
+		// a의 값중 홀수만 c에 저장
+		// (저장위치는 앞에서부터 저장 , 3번 문제와 다름)
+		// 예) b => {5, 3, 1, 0, 0}
+
+		int[] c = { 0, 0, 0, 0, 0 };
+		System.out.println("───────────────────────────────────────────────");
+		System.out.println(" 문제 7) 다음 리스트를 이용해서 a의 값중 홀수만 c에 저장");
+		System.out.println();
+		for (int i = 0; i < c.length; i++) {
+			if (a[i] % 2 == 1) {
+				c[cArrayIndexNumber] = a[i];
+				cArrayIndexNumber++;
+			}
+		}
+
+		System.out.print(" 답 : { ");
+		for (int i = 0; i < c.length; i++) {
+			System.out.print(c[i]);
+			if (i < c.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println(" }");
+
+		sc.close();
+
 	}
 
 }
--- 02_java_array_basic/src/step2_01/array/ArrayEx24_테스트문제1_2.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx24_테스트문제1_2.java
@@ -1,35 +1,131 @@
 package step2_01.array;
 
+import java.util.Scanner;
+
+//2021-03-02 20:10
 public class ArrayEx24_테스트문제1_2 {
 
 	public static void main(String[] args) {
 
+		Scanner sc = new Scanner(System.in);
+		int check = -1;
+		int index = 0;
+		int newIndex = 0;
+		System.out.println("┌─────────────────────────────────────────────┐");
+		System.out.println("│           ArrayEx24_테스트문제1_2           │");
+		System.out.println("└─────────────────────────────────────────────┘");
 		
 		// 문제 8) 아래 d에서 scanner로 내가 입력한 값만 빼고 e에 저장 
+		// 예) 30 ==> e = {10,20,40,50,0};
+		
 		int[] d = { 10,20,30,40,50 };
 		int[] e = { 0,0,0,0,0 };
-		// 예) 30 ==> e = {10,20,40,50,0};
-
+		
+		System.out.println(" 문제 8) 배열 d에서 scanner로 ");
+		System.out.println("         내가 입력한 값만 빼고 e에 저장 ");
+		System.out.println();
+		
+		System.out.print(" { ");
+		for (int i = 0; i < d.length; i++) {
+			System.out.print(d[i]);
+			if (i < d.length - 1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println(" }");
+		
+		System.out.print(" 값 입력 : ");
+		int getValue = sc.nextInt();
+		
+		// 입력한 값이 배열에 있는 값이 맞는지
+		
+		for (int i = 0; i < e.length; i++) {
+			if (getValue == d[i]) {
+				check = 0;
+				index = i;
+			}
+		}
+		
+		if       (check ==  0) {
+			
+			for (int i = 0; i < index; i++) {
+				e[i] = d[i];
+			}
+			
+			for (int i = index; i < 4; i++) {
+				e[i] = d[i + 1];
+			}
+			
+			e[4] = 0;
+			
+			System.out.print(" 답 :  { ");
+			for (int i = 0; i < e.length; i++) {
+				System.out.print(e[i]);
+				if (i < e.length - 1) {
+					System.out.print(", ");
+				}
+			}
+			System.out.println(" }");
+			
+		} else if(check == -1) {
+			
+		}
+		
+		System.out.println();
+		
 		// 문제 9) 아래 f에서 scanner로 내가 입력한 번호와 값을 빼고 d에 저장 
-		int[] f = { 1001, 40, 1002, 65, 1003,  70 };
-		int[] g = { 0,0,0,0,0,0 };
 		// 예) 1002 ==> {1001, 40, 1003, 70 , 0, 0};
 		
+		int[] f = { 1001, 40, 1002, 65, 1003,  70 };
+		int[] g = { 0,0,0,0,0,0 };
+		
+		System.out.println(" 문제 9) 아래 f에서 scanner로 ");
+		System.out.println("         내가 입력한 번호와 값을 빼고 g에 저장 ");
+		System.out.println();
+		
+		
+		
+		
+		
 		// 문제 10) 숫자를 하나 입력받고 아래 배열을 앞으로 하나씩 밀어낸후 맨뒤에 저장
-		int[] h = { 10,20,30,40,50 };
 		// 예)  60 ==> {20,30,40,50,60};
 		
+		int[] h = { 10,20,30,40,50 };
+		
+		System.out.println(" 문제 10) 숫자를 하나 입력받고 아래 배열을 ");
+		System.out.println("          앞으로 하나씩 밀어낸후 맨뒤에 저장");
+		System.out.println();
+		
+		
+		
+		
+		
 		// 문제 11) 숫자를 하나 입력받고 아래 배열을 뒤로 하나씩 밀어낸후 맨 앞에 저장 
-		int[] i = { 10,20,30,40,50 };
 		// 예) 60 ==> {60,10,20,30,40};
 		
+		int[] i = { 10,20,30,40,50 };
+		
+		System.out.println(" 문제 11) 숫자를 하나 입력받고 아래 배열을");
+		System.out.println("          뒤로 하나씩 밀어낸후 맨뒤에 저장");
+		System.out.println();
+		
+		
+		
+		
+		
+		
+		
+		
 		// 문제 12) 아래배열을 거꾸로 저장 
+		//예) k => {5,4,3,2,1};
+		
 		int[] j = { 1,2,3,4,5 };
 		int[] k = { 0,0,0,0,0 };
-		//예) k => {5,4,3,2,1};
-
 		
+		System.out.println(" 문제 12) 아래배열을 거꾸로 저장 ");
+		System.out.println();
 		
+		sc.close();
 		
 	}
 
